99aee707446ffe44060bd2615fbab5159bd6a34e
hqro <glarivie@student.42.fr>
2016-04-04 09:50:10 +0200

    clean myscript

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -1,40 +1,29 @@
-(function() {
-	$('div#request-api button').on({
-		'click' : function(e) {
-			e.preventDefault();
-			console.log('button clicked');
-		},
+// Put event listeners into place
+window.addEventListener("DOMContentLoaded", function() {
+	// Grab elements, create settings, etc.
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		video = document.getElementById("video"),
+		videoObj = { "video": true },
+		errBack = function(error) {
+			console.log("Video capture error: ", error.code);
+		};
+
+	// Put video listeners into place
+	if(navigator.getUserMedia) { // Standard
+		navigator.getUserMedia(videoObj, function(stream) {
+			video.src = stream;
+			video.play();
+		}, errBack);
+	} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+		navigator.webkitGetUserMedia(videoObj, function(stream){
+			video.src = window.URL.createObjectURL(stream);
+			video.play();
+		}, errBack);
+	}
+
+	// Trigger photo take
+	document.getElementById("snap").addEventListener("click", function() {
+		context.drawImage(video, 0, 0, 480, 320);
 	});
-
-	// Put event listeners into place
-	window.addEventListener("DOMContentLoaded", function() {
-		// Grab elements, create settings, etc.
-		var canvas = document.getElementById("canvas"),
-			context = canvas.getContext("2d"),
-			video = document.getElementById("video"),
-			videoObj = { "video": true },
-			errBack = function(error) {
-				console.log("Video capture error: ", error.code);
-			};
-
-		// Put video listeners into place
-		if(navigator.getUserMedia) { // Standard
-			navigator.getUserMedia(videoObj, function(stream) {
-				video.src = stream;
-				video.play();
-			}, errBack);
-		} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-			navigator.webkitGetUserMedia(videoObj, function(stream){
-				video.src = window.URL.createObjectURL(stream);
-				video.play();
-			}, errBack);
-		}
-
-		// Trigger photo take
-		document.getElementById("snap").addEventListener("click", function() {
-			context.drawImage(video, 0, 0, 480, 320);
-		});
-	}, false);
-
-	console.log('App sucessfully loaded !');
-})();
+}, false);
